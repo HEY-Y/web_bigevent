56b67a4f199c1e085d68b85c72696d8e37ed9694
young <923731892@qq.com>
2022-06-19 18:05:24 +0800

    完成了主页功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,19 @@
 // 注意：每次调用$.get()或者$.post()或者$.ajax()的时候，会先调用ajaxPrefilter这个函数，在这个函数中，可以拿到我们给ajax提供的配置对象
 $.ajaxPrefilter(function (options) {
   options.url = "http://www.liulongbin.top:3007" + options.url;
+  if (options.url.indexOf("/my/") !== -1) {
+    //统一为有权限的接口 设置headers请求头
+    options.header = { Authorization: localStorage.getItem("token") || "" };
+  }
+
+  //全局统一挂载complete回调函数
+  options.complete = function (res) {
+    //在complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
+    if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+      //强制清空token
+      localStorage.removeItem("token");
+      //强制跳转到登录页面
+      location.href = "login.html";
+    }
+  };
 });
